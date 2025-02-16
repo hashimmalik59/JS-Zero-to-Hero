e1664054df075f5ebebc1faf50dcd46a6471dc9e
hashimmalik59 <hm3630450@gmail.com>
2025-02-16 20:38:23 +0500

    Values and Variables with Assignment

---
+++ 01_section/03_assignment.js
@@ -0,0 +1,12 @@
+// Values and Variables
+// Assignment
+
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+let myCountry = "Pakistan";
+let myContinent = "Asia";
+let myCountryPopulation = "255.5M";
+
+// Log their values to the console.
+console.log(myCountry);
+console.log(myContinent);
+console.log(myCountryPopulation);
