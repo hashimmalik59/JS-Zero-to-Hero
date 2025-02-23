369061e30c666ebab7a02f14b805f46e7c61fc5a
hashimmalik59 <hm3630450@gmail.com>
2025-02-23 09:22:57 +0500

    Ternary Operator

---
+++ 01_section/16_ternaryOperator.js
@@ -0,0 +1,8 @@
+// Ternary Operator
+// For checking only 2 conditions we use "Ternary Operator"
+
+const myAge = 19;
+//         if                      else
+myAge > 17 ? console.log("Adult!") : console.log("Not Adult!");
+
+console.log(`Now i'm ${myAge > 17 ? "Adult" : "Not Adult"}`); // we can also use it in template literals
