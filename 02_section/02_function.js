23a1258900538dc88fa2dd189aa6d3c7a84258e5
hashimmalik59 <hm3630450@gmail.com>
2025-02-24 21:48:12 +0500

    Function

---
+++ 02_section/02_function.js
@@ -0,0 +1,20 @@
+// Function
+
+// Function is the piece code that we can use over and over again in our code
+
+function name() { // first function keyword and then function name
+    console.log("Hashim"); // statement
+}
+// name(); // invoking / calling / running a function
+// name();
+// name(); // reusing the code over and over again
+
+// name(): "()" these are "parameters" it is like a variable
+
+function fullName(firstName, lastName) { // user defined function
+    return(firstName, lastName); // "return" keyword is for returning a given a value
+}
+fullName(("Hashim", "Malik")); // in invoking "()" it is called "arguments" "Hashim" for firstName and "Malik" for lastName
+
+// console.log() // this is built-in function
+// Nummber() // this is built-in function
