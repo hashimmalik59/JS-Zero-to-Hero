330f1bb25cc29c4bb9045ecaffca0963a54d358a
hashimmalik59 <hm3630450@gmail.com>
2025-03-02 11:19:36 +0500

    Updating the return 'keyword' comments

--- 02_section/02_function.js
+++ 02_section/02_function.js
@@ -12,11 +12,20 @@ function name() { // first function keyword and then function name
 // name(): "("firstName", "lastName")" these are "parameters" it is like a variable
 
 function fullName(firstName, lastName) { // user defined function
-    return(firstName, lastName); // "return" keyword is for returning a given a value
+    const myFullName = `My full name is ${firstName} ${lastName}`
+    return myFullName; // "return" keyword is for returning a given a value
 }
-fullName(("Hashim", "Malik")); // in invoking "("Hashim", "Malik")" it is called "arguments" "Hashim" for firstName and "Malik" for lastName
+// fullName("Hashim", "Malik"); // in invoking "("Hashim", "Malik")" it is called "arguments" "Hashim" for firstName and "Malik" for lastName
+
+// Note: If we want to use the value of "return" then we use it in a variable like that;
+let checkReturn = fullName("Hashim", "Malik");
+console.log(checkReturn);
+
 
 // console.log() // this is built-in function
 // Nummber() // this is built-in function
 
 // Note: We cannot use any code after "return" keyword
+// Why we use:
+// 1) Exits a Function: When "return" is used inside a function, it stops the execution of the function and exits it immediately.
+// 2) Returns a Value: You can use "return" to send a value back to the user of the function.
