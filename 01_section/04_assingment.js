1a45a3b0d6aaf6f3f94b22942f7d7646a7df991d
hashimmalik59 <hm3630450@gmail.com>
2025-03-07 20:26:29 +0500

    Assignment of Values and Variables

---
+++ 01_section/04_assingment.js
@@ -0,0 +1,13 @@
+// Values and Variables
+// Exercise
+
+// Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+// Log the types of isIsland, population, country and language to the console.
+
+const isIsland = true;
+const population = 25756758;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof language);
