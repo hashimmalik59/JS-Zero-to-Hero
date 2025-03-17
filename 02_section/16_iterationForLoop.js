bab7fec69aaaf664198d0345bd2e681df492100e
hashimmalik59 <hm3630450@gmail.com>
2025-03-17 05:52:37 +0500

    Iteration: The for loops

---
+++ 02_section/16_iterationForLoop.js
@@ -0,0 +1,21 @@
+// Iteration: The for loop
+
+// console.log("Weight Lift rep 1");
+// console.log("Weight Lift rep 2");
+// console.log("Weight Lift rep 3");
+// console.log("Weight Lift rep 4");
+// console.log("Weight Lift rep 5");
+// console.log("Weight Lift rep 6");
+// console.log("Weight Lift rep 7");
+// console.log("Weight Lift rep 8");
+// console.log("Weight Lift rep 9");
+// console.log("Weight Lift rep 10");
+
+// for loop keep running while condition is TRUE
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Weight Lift rep ${rep}`);
+}
+
+// step1: Initialization (let rep = 1)
+// step2: Condition (rep <= 10)
+// step3: Updation (rep++)
