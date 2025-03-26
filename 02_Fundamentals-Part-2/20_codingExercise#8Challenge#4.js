df29fff1171e71f0248dc2e3135decac3bfa57e5
hashimmalik59 <hm3630450@gmail.com>
2025-03-26 21:16:33 +0500

    Changing folder name 'Fundamentals Part 2'

--- 02_Fundamentals-Part-2/20_codingExercise#8Challenge#4.js
+++ 02_Fundamentals-Part-2/20_codingExercise#8Challenge#4.js
@@ -5,7 +5,7 @@
 
 // Create an array called bills containing all 10 test bill values.
 
-// Create empty arrays for the tips and the totals (tips and totals)
+// Create empty arrays for the tips and the totals (tips and totals).
 
 // Use the calcTip function we wrote before (included in the starter code) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
 
