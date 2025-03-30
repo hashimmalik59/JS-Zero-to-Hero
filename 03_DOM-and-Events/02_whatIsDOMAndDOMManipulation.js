0dfb684d4352ff04944af8ea271cb233b9091dc2
hashimmalik59 <hm3630450@gmail.com>
2025-03-30 05:04:21 +0500

    What is DOM and DOM Manipulation

---
+++ 03_DOM-and-Events/02_whatIsDOMAndDOMManipulation.js
@@ -0,0 +1,10 @@
+// What's is DOM and DOM Manipulation
+
+// DOM
+// Document Object Model:
+// Structured representation of HTML documents.
+// Allows JS to access HTML elements and styles to manipulate thems
+
+// example: We change HTML attributes, and even CSS styles
+
+// Note: DOM is not the part of JS and also DOM is the part of browser
