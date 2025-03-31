80b413fd24c5540ed4af7a71d16f467638bec035
hashimmalik59 <hm3630450@gmail.com>
2025-04-01 00:39:33 +0500

    Handling Click Events

---
+++ 03_DOM-and-Events/04_handlingClickEvents.js
@@ -0,0 +1,7 @@
+// Handling Click Events
+
+document.querySelector(".check").addEventListener('click', function () {
+    const guess = document.querySelector("guess").value;
+    console.log(guess);
+
+})
